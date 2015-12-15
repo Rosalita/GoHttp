f2cf08cad5960de4d143c3e456b0399cc7fb99f9
Rosie Hamilton <enchantrix@gmail.com>
2015-12-15 21:18:30 +0000

    applied gofmt

--- web.go
+++ web.go
@@ -5,26 +5,26 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  // handler function is of the type http.HandlerFunc
-  // It takes two arguments.
-  // http.Response.Writer assembles the HTTP server's response by writing to it
-  // http.Request is a datastructure that represents the client HTTP request.
+	// handler function is of the type http.HandlerFunc
+	// It takes two arguments.
+	// http.Response.Writer assembles the HTTP server's response by writing to it
+	// http.Request is a datastructure that represents the client HTTP request.
 
-    fmt.Fprintf(w, "Hello I am a webserver and I love %s!", r.URL.Path[1:])
-    // r.URL.Path is the path component of the request URL.
-    // The trailing [1:] means "create a subslice of path from the 1st character to the end."
-    // This drops the leading "/" from the path name
+	fmt.Fprintf(w, "Hello I am a webserver and I love %s!", r.URL.Path[1:])
+	// r.URL.Path is the path component of the request URL.
+	// The trailing [1:] means "create a subslice of path from the 1st character to the end."
+	// This drops the leading "/" from the path name
 }
 
 func main() {
-    // this tells the net/http package to handle all requests made to to the web root "/" with the handler function
+	// this tells the net/http package to handle all requests made to to the web root "/" with the handler function
 
-    http.HandleFunc("/", handler)
-    // then we tell the webserver to listen and serve on port 8080
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	// then we tell the webserver to listen and serve on port 8080
+	http.ListenAndServe(":8080", nil)
 }
